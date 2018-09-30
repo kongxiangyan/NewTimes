8cdf0d29eeb4068ad8f2b8ed98bd185f63490a29
kongxiangyan <1145172089@qq.com>
2018-10-01 00:36:37 +0800

    store modifes on 20181001 0036

--- mix-line-bar.js
+++ mix-line-bar.js
@@ -2,6 +2,10 @@ var mixChart1 = echarts.init(document.getElementById("mix-line-bar"));
 option = {
     tooltip: {
         trigger: 'axis',
+        textStyle:{
+            align:'left'
+        },
+        confine: true,
         axisPointer: {
             type: 'cross',
             crossStyle: {
@@ -12,7 +16,7 @@ option = {
     toolbox: {
     },
     legend: {
-        data:['在线短租市场规模','环比增长率（%）']
+        data:['在线短租市场规模（亿元）','环比增长率（%）']
     },
     xAxis: [
         {
@@ -31,32 +35,49 @@ option = {
             max: 250,
             interval: 50,
             axisLabel: {
-                formatter: '{value} 亿元'
+                formatter: '{value}'
+            }
+        },
+        {
+            type: 'value',
+            name: '比率',
+            min: 0,
+            max: 150,
+            interval: 30,
+            axisLabel: {
+                formatter: '{value}'
             }
         }
-        // ,
-        // {
-        //     type: 'value',
-        //     name: '比率',
-        //     min: 0,
-        //     max: 25,
-        //     interval: 5,
-        //     axisLabel: {
-        //         formatter: '{value} %'
-        //     }
-        // }
     ],
     series: [
         {
-            name:'在线短租市场规模',
+            name:'在线短租市场规模（亿元）',
             type:'bar',
-            data:[5.5, 9.7, 19.2, 42.6, 87.8, 125.2, 169.6]
+            data:[5.5, 9.7, 19.2, 42.6, 87.8, 125.2, 169.6],
+            itemStyle:{
+                normal:{
+                    color:'#830000'
+                },
+                emphasis:{
+                    color:'#004A83'
+                }
+            }
         },
         {
             name:'环比增长率（%）',
             type:'line',
-            // yAxisIndex: 1,
-            data:[undefined, 76.4, 97.9, 121.9, 106.1, 42.6, 35.5]
+            yAxisIndex: 1,
+            data:[undefined, 76.4, 97.9, 121.9, 106.1, 42.6, 35.5],
+            itemStyle:{
+                normal:{
+                    color:'#004A83',
+                    borderColor:'#004a83'
+                },
+                emphasis:{
+                    color:'#830000',
+                    borderColor:'#830000'
+                }
+            }
         }
     ]
 };
